d973885ef95b9490f002d90d559fd50ab94aba68
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-04-20 22:35:48 +0600

    Update Snake.py

---
+++ Project-1-Snake_Game/Snake.py
@@ -0,0 +1,12 @@
+import pygame
+import random
+import time
+
+pygame.init()
+
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (213, 50, 80)
+blue = (50, 153, 213)
+green = (0, 255, 0)
+yellow = (255, 255, 102)
