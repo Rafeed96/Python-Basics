b72f4e3c4dd76fe5e800363cb48ab2d8dcbb18b1
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-04-21 02:47:14 +0600

    Update Snake.py

--- Project-1-Snake_Game/Snake.py
+++ Project-1-Snake_Game/Snake.py
@@ -83,6 +83,16 @@ def gameLoop():
             snake_Head.append(x1)
             snake_Head.append(y1)
 
+            snake_List.append(snake_Head)
+
+            if len(snake_List) > Length_of_snake:
+                del snake_List[0]
+
+            for x in snake_List[:-1]:
+                if x== snake_Head:
+                    game_close = True
+
+
 
 
 white = (255, 255, 255)
