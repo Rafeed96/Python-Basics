228a1c196d358820b66298bd404b5ccffa23a0e7
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-04-21 01:49:39 +0600

    Update Snake.py

--- Project-1-Snake_Game/Snake.py
+++ Project-1-Snake_Game/Snake.py
@@ -46,7 +46,20 @@ def gameLoop():
             pygame.display.update()
 
             for event in pygame.event.get():
-                
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        game_over = True
+                        game_close = False
+                    if event.key == pygame.K_c:
+                        gameLoop()
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    game_over = True
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        x1_change = -snake_block
+                        y1_change = 0
 
 
 white = (255, 255, 255)
