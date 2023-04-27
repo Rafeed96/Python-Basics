aa4cb4e40b856882454154cd43840149416de421
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-04-28 05:53:06 +0600

    Update SpaceDodge.py

--- Project 5 - Space Dodge/SpaceDodge.py
+++ Project 5 - Space Dodge/SpaceDodge.py
@@ -1,30 +1,62 @@
 import pygame
 import time
 import random
+pygame.font.init()
 
-WIDTH, HEIGHT = 600, 400
+WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 pygame.display.set_caption("Space Dodger")
 
-BG = pygame.image.load("SBG.jpg")
+BG = pygame.transform.scale(pygame.image.load("SBG.jpg"), (WIDTH, HEIGHT))
+
+FONT = pygame.font.SysFont("comicsans", 30)
 
 
-def draw():
+def draw(player, elapsed_time):
     WIN.blit(BG, (0, 0))
+
+    time_text = FONT.render(f"Time: {round(elapsed_time)}s", 1, "white")
+    WIN.blit(time_text, (10, 10))
+
+    pygame.draw.rect(WIN, "white", player)
     pygame.display.update()
+
+
+PLAYER_WIDTH = 40
+PLAYER_HEIGHT = 60
+PLAYER_VELOCITY = 5
 
 
 def main():
     run = True
 
+    player = pygame.Rect(200, HEIGHT-PLAYER_HEIGHT,
+                         PLAYER_WIDTH, PLAYER_HEIGHT)
+
+    clock = pygame.time.Clock()
+
+    start_time = time.time()
+    elapsed_time = 0
+
     while run:
+        clock.tick(60)
+
+        elapsed_time = time.time() - start_time
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
 
-        draw()
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT] and player.x - PLAYER_VELOCITY >= 0:
+            player.x -= PLAYER_VELOCITY
+        if keys[pygame.K_RIGHT] and player.x + PLAYER_VELOCITY + player.width <= WIDTH:
+            player.x += PLAYER_VELOCITY
+
+        draw(player, elapsed_time)
 
     pygame.quit()
 
