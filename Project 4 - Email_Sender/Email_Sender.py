f12fe9bd82e3e9e6ff9ef133ae6f5392369dfcdf
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-04-27 23:22:54 +0600

    Update Email_Sender.py

--- Project 4 - Email_Sender/Email_Sender.py
+++ Project 4 - Email_Sender/Email_Sender.py
@@ -1,6 +1,8 @@
 from email.message import EmailMessage
 from app2 import password
 
+import ssl
+import smtplib
 
 email_sender = "ribrahim173095@bscse.uiu.ac.bd"
 email_password = password
@@ -14,3 +16,12 @@ body = "Lets see if this works or not "
 em = EmailMessage()
 em['From'] = email_sender
 em['To'] = email_receiver
+em['subject'] = subject
+
+em.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
