832333ee8de61bd821b036bfc40caa8c7fbed1ab
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-04-27 22:11:22 +0600

    Project QR Code

---
+++ Project 3 - QR Code/QR_Code.py
@@ -0,0 +1,25 @@
+# install libraries
+# function to collect text and perform conversion to QR code
+# save the QR as an image
+# run the function
+
+import qrcode
+
+
+def generate_qecode(text):
+
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROT_CORRECT_L,
+        box_size=10,
+        border=4,
+
+    )
+
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("qrimg.png")
+
+
+generate_qecode("https://www.youtube.com")
