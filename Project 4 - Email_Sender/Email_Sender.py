6924249e3db6740a36b839128d976b339701fc90
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-04-27 23:16:43 +0600

    Update Email_Sender.py

--- Project 4 - Email_Sender/Email_Sender.py
+++ Project 4 - Email_Sender/Email_Sender.py
@@ -1,2 +1,16 @@
 from email.message import EmailMessage
 from app2 import password
+
+
+email_sender = "ribrahim173095@bscse.uiu.ac.bd"
+email_password = password
+
+email_receiver = ""
+
+subject = "Trial of email sender"
+body = "Lets see if this works or not "
+
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver
