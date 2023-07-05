268b139deeb636deb459f33a121848fe3d979ca5
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-05 21:13:07 +0600

    Update Phone_Number_Location.py

--- Project 6 - Current Locaton Of Phone/Phone_Number_Location.py
+++ Project 6 - Current Locaton Of Phone/Phone_Number_Location.py
@@ -5,3 +5,8 @@ from phonenumbers import timezone, geocoder, carrier
 number = input("Enter your phone number with +880: ")
 
 phone = phonenumbers.parse(number)
+time = timezone.time_zones_for_geographical_number(phone)
+car = carrier.name_for_number(phone, "en")
+reg = geocoder.description_for_number(phone, "en")
+
+
