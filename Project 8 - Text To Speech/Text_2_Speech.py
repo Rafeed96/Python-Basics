e3624f7fa1fc379bf22d7739f5d7de3cdd97cfd3
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-05 23:29:54 +0600

    Update Text_2_Speech.py

--- Project 8 - Text To Speech/Text_2_Speech.py
+++ Project 8 - Text To Speech/Text_2_Speech.py
@@ -6,8 +6,16 @@ import pyttsx3
 
 root = Tk()
 
+textv = StringVar()
+
 obj = LabelFrame(root,text="Text To Speech", font=20, bd=1)
-obj.pack(fill="both", expand="yes", padx=10, pady=)
+obj.pack(fill="both", expand="yes", padx=10, pady=10)
+
+lbl = Label(obj, text="Text", font=30)
+lbl.pack(side=tk.LEFT, padx=5)
+
+text=Entry(obj,textvariable=textv, font=30, width=25, bd=5)
+text.pack(side=tk.LEFT,padx=10)
 
 
 root.title("Text To Speech")
