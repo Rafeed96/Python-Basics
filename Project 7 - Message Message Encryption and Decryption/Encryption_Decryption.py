87674811b954566c73878ac95c5f1aa8808bc00d
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-05 23:17:36 +0600

    Update Encryption_Decryption.py

--- Project 7 - Message Message Encryption and Decryption/Encryption_Decryption.py
+++ Project 7 - Message Message Encryption and Decryption/Encryption_Decryption.py
@@ -11,10 +11,12 @@ def main_screen():
     screen.geometry("375x398")
 
     #icon
-    image_icon = PhotoImage(file="ico.img")
+    image_icon = PhotoImage(file="E:\Projects\Repositories\Python\Python-Basics\Project 7 - Message Message Encryption and Decryption\ico.jpg")
     screen.iconphoto(False, image_icon)
 
     Label(text="Enter text for encryption and decryption", fg="black", font=("calibri",13)).place(x=10,y=10)
+    text1 = Text(font="Roboto 20", bg="white", relief=GROOVE, wrap=WORD.bd=0)
+    text1.place(x=10,y=50,width=355,height=100)
 
     screen.title("PctApp")
 
