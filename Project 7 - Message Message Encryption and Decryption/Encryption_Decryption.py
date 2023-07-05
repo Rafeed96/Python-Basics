ffd69892d711e73f51e1f62bc5813a32389065e7
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-05 22:38:22 +0600

    Update Encryption_Decryption.py

--- Project 7 - Message Message Encryption and Decryption/Encryption_Decryption.py
+++ Project 7 - Message Message Encryption and Decryption/Encryption_Decryption.py
@@ -5,4 +5,13 @@ from tkinter import messagebox
 import base64
 import os
 
+def main_screen():
+
+    screen = Tk()
+    screen.geometry("375x398")
+
+    screen.title("PctApp")
+
+    screen.mainloop()
+
 
