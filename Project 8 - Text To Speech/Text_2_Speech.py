b6436bbaecdb1d2c37337fdc9067c1e56452b843
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-05 23:23:13 +0600

    Update Text_2_Speech.py

--- Project 8 - Text To Speech/Text_2_Speech.py
+++ Project 8 - Text To Speech/Text_2_Speech.py
@@ -4,8 +4,11 @@ import tkinter as tk
 from tkinter import *
 import pyttsx3
 
-
 root = Tk()
+
+obj = LabelFrame(root,text="Text To Speech", font=20, bd=1)
+obj.pack(fill="both", expand="yes", padx=10, pady=)
+
 
 root.title("Text To Speech")
 root.geometry("400x200")
