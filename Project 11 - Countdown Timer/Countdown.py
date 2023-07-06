4d03d56c0e14fb13fcfd5b605cd886a6e516a4e6
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-06 21:50:59 +0600

    Update Countdown.py

---
+++ Project 11 - Countdown Timer/Countdown.py
@@ -0,0 +1,13 @@
+# Install playsound package
+
+from tkinter import *
+from playsound import playsound
+import time
+
+root=Tk()
+root.title("Time Countdown")
+root.geometry("400x600")
+root.config(bg="#000")
+root.resizable(False,False)
+
+root.mainloop()
