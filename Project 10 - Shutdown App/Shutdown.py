5232ac42c0a3a99d8347481712494fe2ee6b864f
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-06 21:35:46 +0600

    Update Shutdown.py

--- Project 10 - Shutdown App/Shutdown.py
+++ Project 10 - Shutdown App/Shutdown.py
@@ -5,3 +5,15 @@ root=Tk()
 root.title("Shutdown App")
 root.geometry("400x580")
 
+# Restart Button
+restart_time_button = PhotoImage(file="Project 10 - Shutdown App\redo-refresh-restart.jpg")
+first_botton = Button(root, image=restart_time_button, borderwidth=0, cursor="hand2")
+first_botton.place(x=10, y=10)
+
+# Close Button
+close_button = PhotoImage(file="Project 10 - Shutdown App/close-button-red.jpg")
+second_botton = Button(root, image=close_button, borderwidth=0, cursor="hand2")
+second_botton.place(x=200, y=10)
+
+
+root.mainloop()
