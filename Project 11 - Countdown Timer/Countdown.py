504b33a5da14998f41a271e2bc31257fd31306a0
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-06 21:54:07 +0600

    Update Countdown.py

--- Project 11 - Countdown Timer/Countdown.py
+++ Project 11 - Countdown Timer/Countdown.py
@@ -10,4 +10,10 @@ root.geometry("400x600")
 root.config(bg="#000")
 root.resizable(False,False)
 
+heading = Label(root,text="Timer", font="arial 30 bold", bg="#000", fg="#ea3548")
+heading.pack(pady=10)
+
+Label(root,font=("arial", 15, "bold"), text="Current Time:", bg="papaya whip").place(x=65, y=70)
+            
+
 root.mainloop()
