1151db77656a1bdf509e6b1109f55901e132538b
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-06 21:44:11 +0600

    Update Shutdown.py

--- Project 10 - Shutdown App/Shutdown.py
+++ Project 10 - Shutdown App/Shutdown.py
@@ -7,28 +7,28 @@ root.geometry("400x580")
 
 # Restart Button
 restart_time_button = PhotoImage(file="E:\Projects\Repositories\Python\Python-Basics\Project 10 - Shutdown App\restart.jpg")
-first_botton = Button(root, image=restart_time_button, borderwidth=0, cursor="hand2")
+first_botton = Button(root, image=restart_time_button, borderwidth=0, cursor="hand2", command=restarttime)
 first_botton.place(x=10, y=10)
 
 # Close Button
 close_button = PhotoImage(file="Project 10 - Shutdown App/close-button-red.jpg")
-second_botton = Button(root, image=close_button, borderwidth=0, cursor="hand2")
+second_botton = Button(root, image=close_button, borderwidth=0, cursor="hand2", command=root.destroy)
 second_botton.place(x=200, y=10)
 
 # Restart Button
 restart_button = PhotoImage(file="E:\Projects\Repositories\Python\Python-Basics\Project 10 - Shutdown App\redo-refresh-restart.jpg")
-third_botton = Button(root, image=restart_button, borderwidth=0, cursor="hand2")
+third_botton = Button(root, image=restart_button, borderwidth=0, cursor="hand2", command=restart)
 third_botton.place(x=10, y=200)
 
 # Shutdown Button
 shutdown_button = PhotoImage(file="E:\Projects\Repositories\Python\Python-Basics\Project 10 - Shutdown App\shutdown.jpg")
-fourth_botton = Button(root, image=shutdown_button, borderwidth=0, cursor="hand2")
+fourth_botton = Button(root, image=shutdown_button, borderwidth=0, cursor="hand2", command=shutdown)
 fourth_botton.place(x=200, y=200)
 
 
 # Logout Button
 logout_button = PhotoImage(file="E:\Projects\Repositories\Python\Python-Basics\Project 10 - Shutdown App\logout-mini-icon.jpg")
-fifth_botton = Button(root, image=logout_button, borderwidth=0, cursor="hand2")
+fifth_botton = Button(root, image=logout_button, borderwidth=0, cursor="hand2",command=logout)
 fifth_botton.place(x=10, y=400)
 
 
