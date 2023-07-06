e912c5c0dfd67c9a66f97020be17a3965369496d
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-06 22:37:37 +0600

    Update Countdown.py

--- Project 11 - Countdown Timer/Countdown.py
+++ Project 11 - Countdown Timer/Countdown.py
@@ -28,16 +28,20 @@ clock()
 
 # Timer
 hrs=StringVar()
-Entry(root,textvariable=hrs,width=2,font="arial 50").place(x=30, y=155)
+Entry(root,textvariable=hrs,width=2,font="arial 50", bg="#000", fg="#fff", bd=0).place(x=30, y=155)
 hrs.set("00")
 
 mins=StringVar()
-Entry(root,textvariable=mins,width=2,font="arial 50").place(x=150, y=155)
+Entry(root,textvariable=mins,width=2,font="arial 50", bg="#000", fg="#fff", bd=0).place(x=150, y=155)
 mins.set("00")
 
 sec=StringVar()
-Entry(root,textvariable=sec,width=2,font="arial 50").place(x=270, y=155)
+Entry(root,textvariable=sec,width=2,font="arial 50", bg="#000", fg="#fff", bd=0).place(x=270, y=155)
 sec.set("00")
+
+Label(root,text="hours",font="arial 12", bg="#000", fg="#fff").place(x=105,y=200)
+Label(root,text="mins",font="arial 12", bg="#000", fg="#fff").place(x=225,y=200)
+Label(root,text="sec",font="arial 12", bg="#000", fg="#fff").place(x=345,y=200)
 
 
 root.mainloop()
