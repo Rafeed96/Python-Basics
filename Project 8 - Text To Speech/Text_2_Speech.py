c529952c31e3d94f28ca47b4ee6d960d1fb2dfae
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-06 13:37:14 +0600

    Update Text_2_Speech.py

--- Project 8 - Text To Speech/Text_2_Speech.py
+++ Project 8 - Text To Speech/Text_2_Speech.py
@@ -4,6 +4,13 @@ import tkinter as tk
 from tkinter import *
 import pyttsx3
 
+engine=pyttsx3.init()
+
+def speaknow():
+    engine.say(textv.get())
+    engine.runAndWait()
+    engine.stop()
+
 root = Tk()
 
 textv = StringVar()
@@ -17,7 +24,7 @@ lbl.pack(side=tk.LEFT, padx=5)
 text=Entry(obj,textvariable=textv, font=30, width=25, bd=5)
 text.pack(side=tk.LEFT,padx=10)
 
-btn=Button(obj,text="Speak",font=20,bg="black",fg="white")
+btn=Button(obj,text="Speak",font=20,bg="black",fg="white", command=speaknow)
 btn.pack(side=tk.LEFT,padx=10)
 
 root.title("Text To Speech")
