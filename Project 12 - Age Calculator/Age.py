3d6c48c9cc1ff7f2785d845f2a5f2e29fd1ca4dd
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-07 22:21:59 +0600

    Updated

---
+++ Project 12 - Age Calculator/Age.py
@@ -0,0 +1,10 @@
+# Import packages
+from tkinter import *
+from datetime import date
+
+# Set GUI window
+
+root=Tk()
+root.geometry("800x600")
+root.resizable(False,False)
+root.title("Age Calculator")
