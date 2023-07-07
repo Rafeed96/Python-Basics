1619d6533419d18b3a8311d06f572b94728872fe
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-07 23:22:09 +0600

    Update BMI.py

--- Project 13 - BMI Calculator/BMI.py
+++ Project 13 - BMI Calculator/BMI.py
@@ -4,3 +4,14 @@ from tkinter import *
 import tkinter as tk
 from tkinter import tkk
 from PIL import Image, ImageTk
+
+
+# Create BMI window
+
+root=Tk()
+root.title("BMI Calculator")
+root.geometry("470x580+300+200")
+root.resizable(False,False)
+root.configure(bg="#f0f1f5")
+
+root.mainloop()
