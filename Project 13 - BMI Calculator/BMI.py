03105dafe4cd0481daef98c73128d6d541a75a4c
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-07 23:43:58 +0600

    Update BMI.py

--- Project 13 - BMI Calculator/BMI.py
+++ Project 13 - BMI Calculator/BMI.py
@@ -2,7 +2,7 @@
 
 from tkinter import *
 import tkinter as tk
-from tkinter import tkk
+from tkinter import ttk
 from PIL import Image, ImageTk
 
 
@@ -13,5 +13,16 @@ root.title("BMI Calculator")
 root.geometry("470x580+300+200")
 root.resizable(False,False)
 root.configure(bg="#f0f1f5")
+
+# icon
+image_icon=PhotoImage(file="")
+root.iconphoto(False,image_icon)
+
+# Top
+
+top= PhotoImage(file="")
+top_image=Label(root,image=top,background="#f0f1f5")
+top_image.place(x=-10, y=-10)            
+
 
 root.mainloop()
