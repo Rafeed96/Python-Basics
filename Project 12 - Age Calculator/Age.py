0cf9602a585ae237a7bd26f904d0170e3319e705
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-07 22:50:29 +0600

    Update Age.py

--- Project 12 - Age Calculator/Age.py
+++ Project 12 - Age Calculator/Age.py
@@ -9,6 +9,13 @@ root.geometry("800x600")
 root.resizable(False,False)
 root.title("Age Calculator")
 
+def calculateAge():
+    today = date.today()
+    birthDate = date(int(yearEntry.get()), int(monthEntry.get()), int(dayEntry.get()))
+    age=today.year - birthDate.year-((today.month,today.day)<(birthDate.month,birthDate.day))
+    Label(text=f"{nameValue.get()} your age is {age}", font=30).place(x=300,y=500)
+
+
 photo=PhotoImage(file="Project 12 - Age Calculator\AgeCalculator.png")
 myimage=Label(image=photo)
 myimage.pack(padx=15,pady=15)
@@ -34,5 +41,7 @@ monthEntry.place(x=300,y=350)
 
 dayEntry=Entry(root,textvariable=dayValue,width=30,bd=3,font=20)
 dayEntry.place(x=300,y=400)
+
+Button(text="Calculate Age", font=20, bg="black", fg="white", width=11, height=2, command=calculateAge).place(x=300,y=450)
 
 root.mainloop()
