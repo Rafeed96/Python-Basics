af7505780bced43f74c7d5570310b6d5e751be2f
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-07 22:42:36 +0600

    Update Age.py

--- Project 12 - Age Calculator/Age.py
+++ Project 12 - Age Calculator/Age.py
@@ -13,5 +13,16 @@ photo=PhotoImage(file="Project 12 - Age Calculator\AgeCalculator.png")
 myimage=Label(image=photo)
 myimage.pack(padx=15,pady=15)
 
+Label(text="Name",font=23).place(x=200,y=250)
+Label(text="Year",font=23).place(x=200,y=300)
+Label(text="Month",font=23).place(x=200,y=350)
+Label(text="Day",font=23).place(x=200,y=400)
+
+nameValue = StringVar()
+yearValue = StringVar()
+monthValue = StringVar()
+dayValue = StringVar()
+
+nameEn
 
 root.mainloop()
