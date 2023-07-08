8f4506ea4051171bb3761a42a147c8e89d6262ee
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-08 23:39:17 +0600

    Update music.py

--- Project 15 - Music Player/music.py
+++ Project 15 - Music Player/music.py
@@ -12,4 +12,12 @@ root.geometry("920x670+290+85")
 root.configure(bg="#0f1a2b")
 root.resizable(False,False)
 
+mixer.init()
+
+#icon
+image_icon = PhotoImage(file="Project 15 - Music Player/logo.png")
+root.iconphoto(False,image_icon)
+
+
+
 root.mainloop()
