9a1361770553c2312ddc725384a9102f7d00a2f3
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-08 23:05:19 +0600

    Update dices.py

--- Project 14 - Dices Game/dices.py
+++ Project 14 - Dices Game/dices.py
@@ -5,4 +5,15 @@ root=Tk()
 root.geometry("700x450")
 root.title("Roll Dice")
 
+label=Label(root,text="",font=("times",260))
+
+def roll():
+    dice=['\u2680','\u2681','\u2682','\u2683','\u2684','\u2685']
+    label.configure(text=f'random.choice(dice)')
+
+
+button=Button(root,text="lets roll...",width=40,height=5,font=10,bg="white",bd=2)
+button.pack(padx=10,pady=10)
+
+
 root.mainloop()
