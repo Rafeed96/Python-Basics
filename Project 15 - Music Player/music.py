2847a4a9e090441b786c79bac4f0681963b56b4a
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-08 23:47:18 +0600

    Update music.py

--- Project 15 - Music Player/music.py
+++ Project 15 - Music Player/music.py
@@ -26,4 +26,20 @@ Label(root,image=Top,bg="#0f1a2b").pack()
 Logo=PhotoImage(file="Project 15 - Music Player/logo.png")
 Label(root,image=Logo,bg="#0f1a2b").place(x=65,y=115)
 
+
+#button\
+play_button = PhotoImage(file="Project 15 - Music Player\play.png")
+Button(root,image=play_button,bg="#0f1a2b",bd=0).place(x=100,y=400)
+
+stop_button = PhotoImage(file="Project 15 - Music Player\stop.png")
+Button(root,image=stop_button,bg="#0f1a2b",bd=0).place(x=30,y=500)
+
+resume_button = PhotoImage(file="Project 15 - Music Player\resume.png")
+Button(root,image=resume_button,bg="#0f1a2b",bd=0).place(x=115,y=500)
+
+pause_button = PhotoImage(file="Project 15 - Music Player\pause.png")
+Button(root,image=pause_button,bg="#0f1a2b",bd=0).place(x=200,y=500)
+
+
+
 root.mainloop()
