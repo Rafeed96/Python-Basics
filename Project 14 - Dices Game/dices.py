dbf09495bdc331c072d5fa15f1cd8905f33f4786
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-08 23:11:31 +0600

    Update dices.py

--- Project 14 - Dices Game/dices.py
+++ Project 14 - Dices Game/dices.py
@@ -9,10 +9,11 @@ label=Label(root,text="",font=("times",260))
 
 def roll():
     dice=['\u2680','\u2681','\u2682','\u2683','\u2684','\u2685']
-    label.configure(text=f'random.choice(dice)')
+    label.configure(text=f'{random.choice(dice)}{random.choice(dice)}')
+    label.pack()
 
 
-button=Button(root,text="lets roll...",width=40,height=5,font=10,bg="white",bd=2)
+button=Button(root,text="lets roll...",width=40,height=5,font=10,bg="white",bd=2,command=roll)
 button.pack(padx=10,pady=10)
 
 
