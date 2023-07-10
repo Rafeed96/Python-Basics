71afa3dbbb2bebd50a95ba0cb0b5a55023eee61b
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-10 21:25:28 +0600

    Update digitalWhiteBoard.py

--- Project 16 - White Board/digitalWhiteBoard.py
+++ Project 16 - White Board/digitalWhiteBoard.py
@@ -19,5 +19,10 @@ root.iconphoto(False, image_icon)
 color_box = PhotoImage(file="")
 Label(root,image=color_box,bg="#f2f3f5").place()
 
+eraser=PhotoImage(file="")
+Button(root, image=eraser, bg="#f2f3f5").place(x=30,y=400)
+
+colors=Canvas(root,bg="#ffffff", width=37, height=300, bd=0)
+colors.place(x=30,y=60)
 
 root.mainloop()
