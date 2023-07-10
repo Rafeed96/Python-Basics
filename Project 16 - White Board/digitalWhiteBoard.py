0933eb2852d7e18170e718e99f1839de6cfe1af9
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-10 22:13:51 +0600

    Update digitalWhiteBoard.py

--- Project 16 - White Board/digitalWhiteBoard.py
+++ Project 16 - White Board/digitalWhiteBoard.py
@@ -29,29 +29,29 @@ def display_pallete():
     id = colors.create_rectangle((10,10,30,30),fill="black")
     colors.tag_bind(id, 'Button-1', lambda x: show_color('black'))
     
-    id = colors.create_rectangle((10,40,30,60),fill="black")
-    colors.tag_bind(id, 'Button-1', lambda x: show_color('black'))
+    id = colors.create_rectangle((10,40,30,60),fill="gray")
+    colors.tag_bind(id, 'Button-1', lambda x: show_color('gray'))
 
-    id = colors.create_rectangle((10,70,30,90),fill="black")
-    colors.tag_bind(id, 'Button-1', lambda x: show_color('black'))
+    id = colors.create_rectangle((10,70,30,90),fill="brown4")
+    colors.tag_bind(id, 'Button-1', lambda x: show_color('brown4'))
 
-    id = colors.create_rectangle((10,100,30,120),fill="black")2
-    colors.tag_bind(id, 'Button-1', lambda x: show_color('black'))
+    id = colors.create_rectangle((10,100,30,120),fill="red")
+    colors.tag_bind(id, 'Button-1', lambda x: show_color('red'))
     
-    id = colors.create_rectangle((10,130,30,150),fill="black")
-    colors.tag_bind(id, 'Button-1', lambda x: show_color('black'))
+    id = colors.create_rectangle((10,130,30,150),fill="orange")
+    colors.tag_bind(id, 'Button-1', lambda x: show_color('orange'))
 
-    id = colors.create_rectangle((10,160,30,180),fill="black")
-    colors.tag_bind(id, 'Button-1', lambda x: show_color('black'))
+    id = colors.create_rectangle((10,160,30,180),fill="yellow")
+    colors.tag_bind(id, 'Button-1', lambda x: show_color('yellow'))
 
-    id = colors.create_rectangle((10,190,30,210),fill="black")
-    colors.tag_bind(id, 'Button-1', lambda x: show_color('black'))
+    id = colors.create_rectangle((10,190,30,210),fill="green")
+    colors.tag_bind(id, 'Button-1', lambda x: show_color('green'))
 
-    id = colors.create_rectangle((10,220,30,240),fill="black")
-    colors.tag_bind(id, 'Button-1', lambda x: show_color('black'))
+    id = colors.create_rectangle((10,220,30,240),fill="blue")
+    colors.tag_bind(id, 'Button-1', lambda x: show_color('blue'))
 
-    id = colors.create_rectangle((10,250,30,270),fill="black")
-    colors.tag_bind(id, 'Button-1', lambda x: show_color('black'))
+    id = colors.create_rectangle((10,250,30,270),fill="purple")
+    colors.tag_bind(id, 'Button-1', lambda x: show_color("purple"))
 
 
 
