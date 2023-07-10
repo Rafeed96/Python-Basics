377e74e81e3112dd61906afa57ab5fdb57023cb1
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-10 23:08:24 +0600

    Update color_finder.py

--- Project 17 - Color Finder/color_finder.py
+++ Project 17 - Color Finder/color_finder.py
@@ -5,3 +5,11 @@ from tkinter import filedialog
 from PIL import Image, ImageTk
 from colorthief import ColorThief
 import os
+
+root = Tk()
+root.title("Color picker from image")
+root.geometry("800x470+100+100")
+root.configure(bg="#e4e8eb")
+root.resizable(False,False)
+
+root.mainloop()
