d36bcea7a4e3666d4085466734a0d3b41d08411e
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-10 21:29:35 +0600

    Update digitalWhiteBoard.py

--- Project 16 - White Board/digitalWhiteBoard.py
+++ Project 16 - White Board/digitalWhiteBoard.py
@@ -25,4 +25,11 @@ Button(root, image=eraser, bg="#f2f3f5").place(x=30,y=400)
 colors=Canvas(root,bg="#ffffff", width=37, height=300, bd=0)
 colors.place(x=30,y=60)
 
+
+canvas = Canvas(root,width=930,height=500,background="white",cursor="hand2")
+canvas.place(x=100,y=10)
+
+canvas.bind('<Button-1>')
+canvas.bind('<B1-Motion>')
+
 root.mainloop()
