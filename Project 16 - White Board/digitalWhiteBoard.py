8efb0459cda59c76d3abc8ff86bb873a13de16a0
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-10 21:17:36 +0600

    Update digitalWhiteBoard.py

--- Project 16 - White Board/digitalWhiteBoard.py
+++ Project 16 - White Board/digitalWhiteBoard.py
@@ -5,4 +5,10 @@ from tkinter.colorchooser import askcolor
 from tkinter import ttk
 import tkinter as tk
 
+root = Tk()
+root.title("White Board")
+root.geometry("1050x570+150+50")
+root.configure(bg="#f2f3f5")
+root.resizable(False,False)
 
+root.mainloop()
