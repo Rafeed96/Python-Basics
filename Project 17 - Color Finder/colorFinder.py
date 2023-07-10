c2b73185a80c3eb6f43cf70ab52010e8eae78c22
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-11 04:57:19 +0600

    Update colorFinder.py

--- Project 17 - Color Finder/colorFinder.py
+++ Project 17 - Color Finder/colorFinder.py
@@ -55,4 +55,36 @@ hex5=Label(colors,text="#afb83b", fg="#000", font="arial 12 bold", bg="white")
 hex5.place(x=60,y=215)
 
 
+
+
+
+#color strip 1
+colors=Canvas(frame,bg="#fff",width=150,height=265,bd=0)
+colors.place(x=20,y=90)
+
+id1 = colors.create_rectangle((10,10,50,50), fill="#b8255f")
+id2 = colors.create_rectangle((10,50,50,100), fill="#db4035")
+id3 = colors.create_rectangle((10,100,50,150), fill="#ff9933")
+id4 = colors.create_rectangle((10,150,50,200), fill="#fad000")
+id5 = colors.create_rectangle((10,200,50,250), fill="#afb83b")
+
+
+
+hex1=Label(colors,text="#b8255f", fg="#000", font="arial 12 bold", bg="white")
+hex1.place(x=60,y=15)
+
+hex2=Label(colors,text="#db4035", fg="#000", font="arial 12 bold", bg="white")
+hex2.place(x=60,y=65)
+
+hex3=Label(colors,text="#ff9933", fg="#000", font="arial 12 bold", bg="white")
+hex3.place(x=60,y=115)
+
+hex4=Label(colors,text="#fad000", fg="#000", font="arial 12 bold", bg="white")
+hex4.place(x=60,y=165)
+
+hex5=Label(colors,text="#afb83b", fg="#000", font="arial 12 bold", bg="white")
+hex5.place(x=60,y=215)
+
+
+
 root.mainloop()
