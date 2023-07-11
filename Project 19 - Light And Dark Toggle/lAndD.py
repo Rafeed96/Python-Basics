3c0bf8a4c79d56b4e1b68f4e4dc6d2db3d3548a0
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-11 22:19:51 +0600

    update image files

--- Project 19 - Light And Dark Toggle/lAndD.py
+++ Project 19 - Light And Dark Toggle/lAndD.py
@@ -5,4 +5,10 @@ root.title("Toggle Switch")
 root.geometry("400x600")
 root.config(bg="white")
 
-root.mainloop()
+on=PhotoImage(file="light.png")
+off=PhotoImage(file="dark.png")
+
+button=Button(root,image=on,bd=0,bg="white")
+button.pack(padx=50,pady=50)
+
+root.mainloop() 
