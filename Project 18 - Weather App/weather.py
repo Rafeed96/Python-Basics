1c8907498ba4cf6380cafa1d3fdbf7372bc338a8
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-11 10:59:40 +0600

    Update weather.py

---
+++ Project 18 - Weather App/weather.py
@@ -0,0 +1,12 @@
+# install packages
+
+from tkinter import *
+import tkinter as tk
+from geopy import geocoders
+from geopy.geocoders import Nominatim
+from tkinter import ttk, messagebox
+from timezonefinder import TimezoneFinder
+from datetime import *
+import requests
+import pytz
+from PIL import Image, ImageTk
