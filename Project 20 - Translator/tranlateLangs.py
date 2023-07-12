2f75d55abe8958d30553c470b1348583f612ff49
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-12 21:06:52 +0600

    Update tranlateLangs.py

--- Project 20 - Translator/tranlateLangs.py
+++ Project 20 - Translator/tranlateLangs.py
@@ -10,5 +10,14 @@ root.title("Translator")
 root.geometry("1000x400")
 root.configure(bg="white")
 
+language=googletrans.LANGUAGES
+languageV = list(language.values())
+lang1 = language.keys()
+
+combo1=ttk.Combobox(root,values=languageV,font="Roboto 14", state="r")
+combo1.place(x=110,y=20)
+combo1.set("English")
+
+
 root.mainloop()
 
