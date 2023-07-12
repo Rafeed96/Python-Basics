00408d80c8c97f5f57b7f99a0a5a2c17266446bb
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-12 22:22:28 +0600

    Update tranlateLangs.py

--- Project 20 - Translator/tranlateLangs.py
+++ Project 20 - Translator/tranlateLangs.py
@@ -18,10 +18,16 @@ combo1=ttk.Combobox(root,values=languageV,font="Roboto 14", state="r")
 combo1.place(x=110,y=20)
 combo1.set("English")
 
+label1 = Label(root,text="English",font="segeo 30 bold", bg="white", width=18,bd=5,relief=GROOVE)
+label1.place(x=10,y=50)
+
 
 combo2=ttk.Combobox(root,values=languageV,font="Roboto 14", state="r")
 combo2.place(x=730,y=20)
 combo2.set("Select Language")
+
+label2 = Label(root,text="English",font="segeo 30 bold", bg="white", width=18,bd=5,relief=GROOVE)
+label2.place(x=620,y=50)
 
 root.mainloop()
 
