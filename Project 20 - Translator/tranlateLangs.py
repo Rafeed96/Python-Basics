e17616bfad614a55d88cd417cb9ebcea7798e600
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-12 21:04:36 +0600

    Update tranlateLangs.py

--- Project 20 - Translator/tranlateLangs.py
+++ Project 20 - Translator/tranlateLangs.py
@@ -3,4 +3,12 @@
 from tkinter import *
 from tkinter import ttk,messagebox
 import googletrans
-import textblob
+import textblob 
+
+root=Tk()
+root.title("Translator")
+root.geometry("1000x400")
+root.configure(bg="white")
+
+root.mainloop()
+
