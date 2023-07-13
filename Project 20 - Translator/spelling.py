38b908117f53cb735bdd35c6a84bfea6a436246e
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-13 20:55:42 +0600

    Update spelling.py

--- Project 20 - Translator/spelling.py
+++ Project 20 - Translator/spelling.py
@@ -10,4 +10,11 @@ root.title("Spell Checker")
 root.geometry("700x400")
 root.config(background="#dae6f6")
 
+heading=Label(root,text="Spelling Checker",font=("trebuchet MS", 30,"bold"),bg="#dae6f6", fg="#364971")
+heading.pack(pady=(50,0))
+
+enter_text =Entry(root,justify="center", width=30,font=("poppins", 25),bg="white",border=2)
+enter_text.pack(pady=10)
+enter_text.focus()
+
 root.mainloop()
