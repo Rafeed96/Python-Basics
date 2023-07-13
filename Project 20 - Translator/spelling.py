63c40c5f3d42076bd3955c094a112e55d9c33876
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-13 20:52:54 +0600

    Update spelling.py

--- Project 20 - Translator/spelling.py
+++ Project 20 - Translator/spelling.py
@@ -4,3 +4,10 @@ import tkinter
 from tkinter import *
 from textblob import TextBlob
 
+
+root=Tk()
+root.title("Spell Checker")
+root.geometry("700x400")
+root.config(background="#dae6f6")
+
+root.mainloop()
