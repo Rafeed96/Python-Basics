8e8638cc93027bdd671459431d2b860909dce3e6
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-13 20:57:46 +0600

    Update spelling.py

--- Project 20 - Translator/spelling.py
+++ Project 20 - Translator/spelling.py
@@ -17,4 +17,10 @@ enter_text =Entry(root,justify="center", width=30,font=("poppins", 25),bg="white
 enter_text.pack(pady=10)
 enter_text.focus()
 
+button=Button(root,text="Check",font=("arial",20,"bold"), fg="white", bg="red")
+button.pack()
+
+spell=Label(root,font=("poppins",20),bg="#dae6f6", fg="#364971")
+spell.place(x=350,y=250)
+
 root.mainloop()
