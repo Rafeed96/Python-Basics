9070f43397801b6b92320556ad344528759ee993
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-13 21:02:13 +0600

    Update spelling.py

--- Project 20 - Translator/spelling.py
+++ Project 20 - Translator/spelling.py
@@ -10,6 +10,17 @@ root.title("Spell Checker")
 root.geometry("700x400")
 root.config(background="#dae6f6")
 
+def check_spelling():
+    word=enter_text.get()
+    a=TextBlob(word)
+    right=str(a.correct())
+
+    cs=Label(root,text="Correct text is:",font=("poppins",20),bg="#dae6f6",fg="#364971")
+    cs.place(x=100,y=250)
+    spell.config(text=right)
+
+
+
 heading=Label(root,text="Spelling Checker",font=("trebuchet MS", 30,"bold"),bg="#dae6f6", fg="#364971")
 heading.pack(pady=(50,0))
 
@@ -17,7 +28,7 @@ enter_text =Entry(root,justify="center", width=30,font=("poppins", 25),bg="white
 enter_text.pack(pady=10)
 enter_text.focus()
 
-button=Button(root,text="Check",font=("arial",20,"bold"), fg="white", bg="red")
+button=Button(root,text="Check",font=("arial",20,"bold"), fg="white", bg="red",command=check_spelling)
 button.pack()
 
 spell=Label(root,font=("poppins",20),bg="#dae6f6", fg="#364971")
