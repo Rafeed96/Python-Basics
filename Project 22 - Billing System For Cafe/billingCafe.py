8c6459a0ed3b4a0b6e3521f2105d1402ef9f1b63
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-13 21:12:15 +0600

    Update billingCafe.py

--- Project 22 - Billing System For Cafe/billingCafe.py
+++ Project 22 - Billing System For Cafe/billingCafe.py
@@ -5,4 +5,13 @@ root.title("Billing System")
 root.geometry("1000x500")
 root.resizable(False,False)
 
-root,mainloop()
+Label(text="Billing System", bg="black", fg="white",font=("calibri",33),width=300,height=2).pack()
+
+# Menu Items with Prices
+f=Frame(root,bg="Gray",highlightbackground="black",highlightthickness=1,width=300,height=370)
+f.place(x=10,y=118)
+
+Label(f,text="Menu",font=("Gabriola",40,"bold"),bg="gray", fg="black").place(x=80,y=0)
+
+
+root.mainloop()
