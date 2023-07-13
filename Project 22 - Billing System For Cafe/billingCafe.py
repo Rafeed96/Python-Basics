016ee6d85bcec92b43370f03b88c7d2cf332c13b
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-13 21:19:22 +0600

    Update billingCafe.py

--- Project 22 - Billing System For Cafe/billingCafe.py
+++ Project 22 - Billing System For Cafe/billingCafe.py
@@ -13,5 +13,13 @@ f.place(x=10,y=118)
 
 Label(f,text="Menu",font=("Gabriola",40,"bold"),bg="gray", fg="black").place(x=80,y=0)
 
+Label(f,font=("Lucida Calligraphy",15,"bold"),text="Moghlai Parata ... Tk 60/pc",fg="black", bg="gray").place(x=10,y=80)
+Label(f,font=("Lucida Calligraphy",15,"bold"),text="Daal Puri          ... Tk 05/pc",fg="black", bg="gray").place(x=10,y=110)
+Label(f,font=("Lucida Calligraphy",15,"bold"),text="Aloo Puri          ... Tk 05/pc",fg="black", bg="gray").place(x=10,y=140)
+Label(f,font=("Lucida Calligraphy",15,"bold"),text="Shingara          ... Tk 10/pc",fg="black", bg="gray").place(x=10,y=170)
+Label(f,font=("Lucida Calligraphy",15,"bold"),text="Samosa            ... Tk 10/pc",fg="black", bg="gray").place(x=10,y=200)
+Label(f,font=("Lucida Calligraphy",15,"bold"),text="Tea                   ... Tk 10/pc",fg="black", bg="gray").place(x=10,y=230)
+Label(f,font=("Lucida Calligraphy",15,"bold"),text="Naan Roti          ... Tk 15/pc",fg="black", bg="gray").place(x=10,y=260)
+
 
 root.mainloop()
