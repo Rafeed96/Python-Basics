2d4dc9c6bae467a071dee70a9f2eac8df9325c1a
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-14 21:30:08 +0600

    Update student_db.py

--- Project 23 - Student Database/student_db.py
+++ Project 23 - Student Database/student_db.py
@@ -23,4 +23,26 @@ root.title("Student Registration")
 root.geometry("1250x7700+210+100")
 root.configure(bg="#06283D")
 
+
+file=pathlib.Path("Student_Data.xlsx")
+
+if file.exists():
+    pass
+else:
+    file=Workbook()
+    sheet=file.active
+    sheet["A1"]="Registration Np."
+    sheet["B1"]="Name"
+    sheet["C1"]="Class"
+    sheet["D1"]="Gender"
+    sheet["E1"]="DOB"
+    sheet["F1"]="Date of Registration"
+    sheet["G1"]="Religion"
+    sheet["H1"]="Skill"
+    sheet["I1"]="Father's Name"
+    sheet["J1"]="Mother's Name"
+    sheet["K1"]="Father's Occupation"
+    sheet["L1"]="Mother's Occupation"
+
+
 root.mainloop()
