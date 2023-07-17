90ba8a7ec34e38c9989c494c45c6a59a5cbb0836
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-17 22:01:51 +0600

    Update email_sender.py

--- Project 24 - Email Sender/email_sender.py
+++ Project 24 - Email Sender/email_sender.py
@@ -7,3 +7,8 @@ ob.login("raicobd@gmail.com","tenyasha")
 subject="test python"
 body="I love myself, my computer and python"
 message = "subject:{}\n\n{}".format(subject,body)
+
+
+emaiList = ["rafeed.ibrahim@gmail.com","ribrahim173095@bscse.uiu.ac.bd"]
+
+ob.sendmail("")
