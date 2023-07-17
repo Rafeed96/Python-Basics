63aaee1f77110cf73b421621613681b39463675c
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-17 23:50:10 +0600

    Update email_Is_Valid.py

--- Project 26 - Email Validity/email_Is_Valid.py
+++ Project 26 - Email Validity/email_Is_Valid.py
@@ -3,8 +3,14 @@ email = input("Enter Your Email:")
 if len(email)>= 6:
     if email[0].isalpha():
         if "@" in email and (email.count("a")==1):
-            pass
-
+            if (email[-4]==".") ^ (email[-3]=="."):
+                for i in email:
+                    if i == i.isspace():
+                        
+            else:
+                print(" Wrong Email 4")
+        else:
+            print(" Wrong Email 3")
     else:
         print(" Wrong Email 2")
 else:
