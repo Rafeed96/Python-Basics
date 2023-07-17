6ffa55445dfda4ce7d8e9313e246fc626000a344
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-17 23:56:48 +0600

    Update email_Is_Valid.py

--- Project 26 - Email Validity/email_Is_Valid.py
+++ Project 26 - Email Validity/email_Is_Valid.py
@@ -6,7 +6,15 @@ if len(email)>= 6:
             if (email[-4]==".") ^ (email[-3]=="."):
                 for i in email:
                     if i == i.isspace():
+                        k = 1
+                    elif i.isalpha():
+                        if i==i.upper():
+                            j==1
+                    elif i.isdigit():
+                        continue
+                    elif i =="_" or i=="," or i=="@":
                         
+
             else:
                 print(" Wrong Email 4")
         else:
