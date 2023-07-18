099bbaf5f4a62a4e4c4a7545b47844be2eb9ccb3
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-18 20:36:04 +0600

    Update login_form.py

--- Project 27 - Login Form/login_form.py
+++ Project 27 - Login Form/login_form.py
@@ -2,3 +2,13 @@
 from tkinter import *
 from tkinter import messagebox
 
+root=Tk()
+root.title("Login")
+root.geometry("925x500+300+200")
+root.configure(bg="#fff")
+root.resizable(False,False)
+
+
+
+
+root.mainloop()
