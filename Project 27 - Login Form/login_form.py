4498bb07699b16ccdcba5bab05af232775e6b295
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-18 21:28:07 +0600

    Update login_form.py

--- Project 27 - Login Form/login_form.py
+++ Project 27 - Login Form/login_form.py
@@ -11,8 +11,10 @@ root.resizable(False,False)
 img = PhotoImage("Project 27 - Login Form\vecteezy_cloud-computing-modern-flat-concept-for-web-banner-design_5879539.jpg")
 Label(root,image=img,bg="red").place(x=50,y=50)
 
-frame =Frame(root,width=350,height=350,bg="red")
+frame =Frame(root,width=350,height=350,bg="white")
 frame.place(x=480,y=70)
 
+heading =Label(frame,text="Sign In",fg="#57a1f8",bg="white",font=("Microsoft Yahei UI Light",23,"bold"))
+heading.place(x=100,y=5)
 
 root.mainloop()
