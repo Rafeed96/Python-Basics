aea81c924107a6b11ed717b38b9b4253def23d6c
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-18 21:26:01 +0600

    Update login_form.py

--- Project 27 - Login Form/login_form.py
+++ Project 27 - Login Form/login_form.py
@@ -8,6 +8,11 @@ root.geometry("925x500+300+200")
 root.configure(bg="#fff")
 root.resizable(False,False)
 
+img = PhotoImage("Project 27 - Login Form\vecteezy_cloud-computing-modern-flat-concept-for-web-banner-design_5879539.jpg")
+Label(root,image=img,bg="red").place(x=50,y=50)
+
+frame =Frame(root,width=350,height=350,bg="red")
+frame.place(x=480,y=70)
 
 
 root.mainloop()
