72236d23ade3cb8e90b4866565612241a89d4c00
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-18 21:30:34 +0600

    Update login_form.py

--- Project 27 - Login Form/login_form.py
+++ Project 27 - Login Form/login_form.py
@@ -17,4 +17,9 @@ frame.place(x=480,y=70)
 heading =Label(frame,text="Sign In",fg="#57a1f8",bg="white",font=("Microsoft Yahei UI Light",23,"bold"))
 heading.place(x=100,y=5)
 
+user = Entry(frame,width=25,fg="black",border=0,bg="white",font=("Microsoft Yahei UI Light",11))
+user.place(x=30,y=80)
+user.insert(0,"Username")
+
+
 root.mainloop()
