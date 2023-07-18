d9bcc0f34ea923d0a7b69f6886bd0d9878a79ece
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-18 22:12:26 +0600

    Update login_form.py

--- Project 27 - Login Form/login_form.py
+++ Project 27 - Login Form/login_form.py
@@ -15,7 +15,14 @@ def signin():
     password=code.get()
 
     if username=="admin" and password=="1234":
-        print("Hello Admin")
+        screen=Toplevel(root)
+        screen.title("App")
+        screen.geometry("925x500+300+200")
+        screen.config(bg="white")
+
+        Label(screen,text="Hello Everyone",bg="fff", font=("Calibri(Body)", 50,"bold")).pack(expand=True)
+
+        screen.mainloop()
 
 
 # Image for form
