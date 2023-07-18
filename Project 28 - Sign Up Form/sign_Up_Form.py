a812dbd65b4d5de3ca62805e4687ec51e5a6c83c
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-18 22:31:19 +0600

    Update sign_Up_Form.py

---
+++ Project 28 - Sign Up Form/sign_Up_Form.py
@@ -0,0 +1,13 @@
+# Import packages for gui
+
+from tkinter import *
+from tkinter import messagebox
+import ast
+
+window=Tk()
+window.title("Sign Up")
+window.geometry("925x500+300+200")
+window.configure(bg="#fff")
+window.resizable(False,False)
+
+window.mainloop()
