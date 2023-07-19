551785a3cd34b22d4ccece3c5ee5ba534122a7d4
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-19 22:37:35 +0600

    Update digitalClock.py

--- Project 29 - Digital Clock/digitalClock.py
+++ Project 29 - Digital Clock/digitalClock.py
@@ -6,7 +6,10 @@ clock.geometry("1000x500")
 clock.config(bg="darkgray")
 
 label_hr=Label(clock,text="00", font=("times New Roman",60,"bold"),bg="black",fg="white")
-label_hr.place(x=120,y=40,height=110,width=100)
+label_hr.place(x=120,y=50,height=110,width=100)
+label_hr_txt=Label(clock,text="00", font=("times New Roman",60,"bold"),bg="black",fg="white")
+label_hr.place(x=120,y=45,height=110,width=100)
+
 
 label_min=Label(clock,text="00", font=("times New Roman",60,"bold"),bg="black",fg="white")
 label_min.place(x=340,y=40,height=110,width=100)
