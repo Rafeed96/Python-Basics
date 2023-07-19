1afa2cba44e8a13cd2a88a78fcb456c1d73187ca
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-19 23:27:41 +0600

    Update imageFilterSketch.py

--- Project 30 - Image to Pencil Sketch/imageFilterSketch.py
+++ Project 30 - Image to Pencil Sketch/imageFilterSketch.py
@@ -1,6 +1,7 @@
 # Import packages
 from tkinter import *
 from tkinter import Tk, ttk
+from PIL import Image, ImageTk
 
 # Black and white colours 
 
@@ -16,5 +17,10 @@ window.resizable(width=False,height=False)
 
 style = ttk.Style(window)
 style.theme_use("clam")
+
+app_img = Image.open("")
+app_img
+
+
 
 window.mainloop()
