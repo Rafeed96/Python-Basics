c66f664c6bb02e4773e02366fa09e1756db901ce
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-19 23:25:46 +0600

    Update imageFilterSketch.py

--- Project 30 - Image to Pencil Sketch/imageFilterSketch.py
+++ Project 30 - Image to Pencil Sketch/imageFilterSketch.py
@@ -8,3 +8,13 @@ co0 = "#ffffff"
 co1 = "#000000"
 co2 = "#63b9ff"
 
+window = Tk()
+window.title("Sketch")
+window.geometry("300x356")
+window.config(bg=co0)
+window.resizable(width=False,height=False)
+
+style = ttk.Style(window)
+style.theme_use("clam")
+
+window.mainloop()
