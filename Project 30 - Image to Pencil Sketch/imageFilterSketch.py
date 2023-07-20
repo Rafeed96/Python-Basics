533afb0d6f0250bf3665047dae61740a82307752
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-20 20:48:17 +0600

    Update imageFilterSketch.py

--- Project 30 - Image to Pencil Sketch/imageFilterSketch.py
+++ Project 30 - Image to Pencil Sketch/imageFilterSketch.py
@@ -18,9 +18,11 @@ window.resizable(width=False,height=False)
 style = ttk.Style(window)
 style.theme_use("clam")
 
-app_img = Image.open("")
-app_img
+app_img = Image.open("Project 30 - Image to Pencil Sketch\imagePencil.png")
+app_img = app_img.resize((50,50))
+app_img =ImageTk.PhotoImage(app_img)
 
+app_logo = Label(window,image=app_img,text="Image to Pencil")
 
 
 window.mainloop()
